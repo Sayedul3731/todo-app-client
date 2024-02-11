7d607367260d235777a01a03fced5024c07d081e
MD SAYEDUL HAQUE <125432652+Sayedul3731@users.noreply.github.com>
2024-02-11 22:24:20 +0600

    deleted functionality added

--- src/page/taskManagement/AllTodos.jsx
+++ src/page/taskManagement/AllTodos.jsx
@@ -1,21 +1,43 @@
 /* eslint-disable react/no-unescaped-entities */
 import useTodos from "../../hooks/useTodos";
-
+import { MdDeleteOutline } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 
 const AllTodos = () => {
-  const [todos] = useTodos();
+  const [todos, refetch] = useTodos();
+  const axiosPublic = useAxiosPublic();
   console.log(todos);
+
+  const handleDelete = (id) => {
+    console.log(id);
+    axiosPublic.delete(`/todos/${id}`)
+    .then(res => {
+        console.log(res.data);
+        if (res.data.deletedCount > 0) {
+            Swal.fire({
+                title: "Success!",
+                text: "Todo Deleted Successfully.",
+                icon: "success"
+            });
+            refetch()
+        }
+    })
+  }
   return (
     <div>
       <h1 className="text-center mb-3">All Todo's</h1>
       <div className="row row-cols-3 pb-5" style={{ gap: "20px", textAlign: "center", justifyContent: 'center' }}>
         {
-          todos.map(todo => <div key={todo._id} className="card" style={{ width: "18rem" }}>
+          todos?.map(todo => <div key={todo._id} className="card" style={{ width: "18rem" }}>
             <img src={todo.image} className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{todo.title}</h5>
               <p className="card-text">{todo.description}</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+              <a href="#" className={`${todo.status === "completed" ? "btn btn-success" : "btn btn-danger"}`}>{todo?.status}</a>
+              <span style={{fontSize: "30px"}}><FaRegEdit></FaRegEdit></span>
+              <span onClick={() => handleDelete (todo._id)} style={{fontSize: "30px", cursor: "pointer"}}><MdDeleteOutline></MdDeleteOutline></span>
             </div>
           </div>)
         }
