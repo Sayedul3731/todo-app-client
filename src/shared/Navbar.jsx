028ea92e3941ed6205c4b7ac8e0b8a097f54a9a3
MD SAYEDUL HAQUE <125432652+Sayedul3731@users.noreply.github.com>
2024-02-11 12:27:29 +0600

    navbar added with responsive

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -2,30 +2,28 @@ import "./style.css"
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#" style={{ color: "white" }}>Navbar w/ text</a>
-                <button   className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" ></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarText">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#" style={{ color: "white" }}>Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" style={{ color: "white" }}>Features</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" style={{ color: "white" }}>Pricing</a>
-                        </li>
-                    </ul>
-                    <span className="navbar-text" style={{ color: "white" }}>
-                        Navbar text with an inline element
-                    </span>
+        <div className="bg-primary">
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" ></span>
+                    </button>
+                    <div className="collapse navbar-collapse " id="navbarText">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#" style={{ color: "white" }}>Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" style={{ color: "white" }}>Task Management</a>
+                            </li>
+                        </ul>
+                        <span className="navbar-text" style={{ color: "white" }}>
+                            Login
+                        </span>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     );
 };
 
